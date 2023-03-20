f2ceb354d454fef2b876c5c542b5ea0dd8aa6c41
Peeranat Keelapang <peeranat@Peeranats-MacBook-Pro.local>
2023-03-20 23:24:51 +0700

    update server cors

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const cors = require('cors');
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000","https://drug-prescription-tool.vercel.app"],
     methods: ["GET", "POST"],
     credentials: true,
   })
